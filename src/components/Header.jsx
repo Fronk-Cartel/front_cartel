082331815ee607cf84ff64fc18b54703073f8493
Laye <tlayefaah@gmail.com>
2023-12-29 15:24:14 +0100

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="min-h-[10vh] flex items-center justify-center">
-      <nav className="container mx-auto w-full bx-red-400 flex justify-center items-center gap-9 text-base">
+      <nav className="container mx-auto w-full bx-red-400 flex justify-center items-center gap-9 text-sm">
         <ul className="flex gap-10 bg-red-0">
           <li>
             <a href="https://fronk-cartel.vercel.app/">HOME</a>
