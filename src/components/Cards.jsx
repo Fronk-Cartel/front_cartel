0c3e4eca56ad1e1178f06dd567cba3fa2cade90a
Laye <tlayefaah@gmail.com>
2024-01-01 10:20:41 +0100

    rendered pictures

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,9 @@
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import Image from "next/image";
-// import img from "public/assets/image/Fronk Cartel #1.png";
-import pic from "../../public/assets/images/300.png";
+// import pic from "../../public/assets/images/300.png";
 
-export default function Cards() {
+export default function Cards({ info }) {
   const cardRef = useRef(null);
 
   useEffect(() => {
@@ -26,21 +25,21 @@ export default function Cards() {
   return (
     <div ref={cardRef} className="cards overflow-hidden">
       <div>
-        <p className="text-primary font-bold px-2 py-1">#123</p>
+        <p className="text-primary font-bold px-2 py-1 text-sm text-center">{info.name}</p>
       </div>
       <div>
         <Image
-          src={pic}
+          src={`/assets/images${info.image}`}
           width={500}
           height={500}
           alt="hello"
-        //   priority
+          //   priority
           loading="lazy"
           className="w-full h-full object-contain"
         />
       </div>
       <div>
-        <p className="text-primary font-bold px-2 py-2 text-sm">Hello boss</p>
+        <p className="text-primary font-bold px-2 py-2 text-sm"></p>
       </div>
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,68 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Layout from "@/Layout";
 import { gsap } from "gsap";
 import Cards from "@/components/Cards";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const ITEMS_PER_PAGE = 100;
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  const fetchData = async (page) => {
+    try {
+      const startIndex = (page - 1) * ITEMS_PER_PAGE;
+      const endIndex = startIndex + ITEMS_PER_PAGE;
+
+      const response = await fetch("/_metadata.json");
+      const jsonData = await response.json();
+      setTotalPages(Math.ceil(jsonData?.length / ITEMS_PER_PAGE));
+      setData(jsonData.slice(startIndex, endIndex));
+      // console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const renderData = () => {
+    return data?.map((d) => {
+      return <Cards key={d.dna} info={d} />;
+    });
+  };
+  // console.log(data);
+
+  //////////////pagination
+  // console.log(totalPages);
+
+  const renderPaginationButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <button
+          key={i}
+          onClick={() => setCurrentPage(i)}
+          className={`${
+            currentPage === i ? "bg-[#111]" : "bg-[#]"
+          } border p-4 w-5 h-5 text-white justify-center items-center flex rounded-md`}
+        >
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  };
+
+  // console.log(renderData());
+
   useEffect(() => {
     const reveal = gsap.fromTo(
       "input, h2",
-      {  opacity: 0},
+      { opacity: 0 },
       {
         // scale: 1,
         opacity: 1,
@@ -21,6 +76,7 @@ export default function Home() {
       reveal.kill();
     };
   }, []);
+
   return (
     <Layout title="Search" className={` bg-primary`}>
       <div className="container mx-auto bg-blac">
@@ -32,27 +88,16 @@ export default function Home() {
         </section>
 
         <section className="pb-20">
-          <div className="image-gallery mt-10">
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-            <Cards />
-          </div>
+          <div className="image-gallery mt-10">{renderData()}</div>
 
-          <div className="flex justify-center items-center mt-14 space-x-5">
-            <div className="bg-[#] border p-4 w-5 h-5 text-white justify-center items-center flex rounded-md">
+          <div>
+            <div className="flex items-center justify-between mt-14 mb-2 px-3">
+              <div className="arr"> &larr; </div>
+              <div> &rarr; </div>
+            </div>
+            <div className="flex justify-start items-center px-3  space-x-5 overflow-x-scroll pb-4 hide-scroll">
+              {renderPaginationButtons()}
+              {/* <div className="bg-[#] border p-4 w-5 h-5 text-white justify-center items-center flex rounded-md">
               1
             </div>
             <div className="bg-[#111] p-4 border border-black w-6 h-6  text-white justify-center items-center flex rounded-md">
@@ -66,6 +111,7 @@ export default function Home() {
             </div>
             <div className="bg-[#] border p-4 w-5 h-5 text-white justify-center items-center flex rounded-md">
               5
+            </div> */}
             </div>
           </div>
         </section>
